1f50e27dc5fcb82d0be8fb09d12150cdb3c61ce6
LobnaWafe <lobnawafe381@gmail.com>
2026-03-16 14:36:49 +0200

    local fatma_work

---
+++ lib/service_provider/auth/data/s_p_regester_model.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+enum Gender { male, female }
+
+extension GenderExtension on Gender {
+  int get value {
+    switch (this) {
+      case Gender.male:
+        return 1;
+      case Gender.female:
+        return 2;
+    }
+  }
+}
+
+class RegisterRequestModel {
+   File image;
+   String firstName;
+   String lastName;
+   String phone;
+   String password;
+   String specialization;
+  double? price;
+   Gender gender;
+   List<File> certificates;
+   double lat;
+   double lng;
+   String email;
+
+  RegisterRequestModel({
+    required this.image,
+    required this.firstName,
+    required this.lastName,
+    required this.phone,
+    required this.password,
+    required this.specialization,
+    this.price,
+    required this.gender,
+    required this.certificates,
+    required this.lat,
+    required this.lng,
+    required this.email,
+  });
+}
