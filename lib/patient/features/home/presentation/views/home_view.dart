396247e3365fd9e7aa141cda843870958b41e32a
LobnaWafe <lobnawafe381@gmail.com>
2026-03-26 02:52:42 +0200

    cach theme + handle big providers

--- lib/patient/features/home/presentation/views/home_view.dart
+++ lib/patient/features/home/presentation/views/home_view.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grad_project/core/utils/get_it.dart';
+import 'package:grad_project/patient/features/home/categories/repo/categories_repo.dart';
 import 'package:grad_project/patient/features/home/presentation/views/mobile_home_view.dart';
 import 'package:grad_project/patient/features/home/presentation/views/tablet_home_view.dart';
+import 'package:grad_project/patient/features/home/categories/view_model/service_providers_list_cubit/service_providers_list_cubit.dart';
+
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // ناخد عرض الشاشة
     double screenWidth = MediaQuery.of(context).size.width;
 
-    // نحدد أي Widget نعرضه حسب العرض
-    if (screenWidth > 550) {
-      // لو أكبر من 500 → Tablet Layout
-      return const TabletHomeView();
-    } else {
-      // لو أصغر أو يساوي 500 → Mobile Layout
-      return  MobileHomeView();
-    }
+    return BlocProvider(
+      create: (context) => ServiceProvidersListCubit(
+        repo:getIt<CategoriesRepo>()
+      ),
+      child: screenWidth > 550
+          ? const TabletHomeView()
+          : MobileHomeView(),
+    );
   }
 }
